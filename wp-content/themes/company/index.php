96214f5de241544054e5125bc478788b0332d0d2
Kouta Yoshimura <quetz.design@gmail.com>
2013-12-21 19:35:45 +0900

    Chapter4 Lesson01

--- wp-content/themes/company/index.php
+++ wp-content/themes/company/index.php
@@ -1,6 +1,25 @@
 <?php get_header(); ?>
 <div id="container">
-	<div id="contents"></div>
+	<div id="contents">
+		<div id="news" class="box">
+			<div class="section-header">
+				<h2><img src="<?php bloginfo('template_url'); ?>/images/ttl_news.gif" alt=""></h2>
+				<p><a href="<?php bloginfo('url'); ?>">一覧を見る</a></p>
+			</div>
+			<?php if(have_posts()): ?>
+			<dl>
+				<?php while(have_posts()): the_post(); ?>
+				<dt><?php the_time('Y/m/d'); ?></dt>
+				<?php
+					$cats = get_the_category();
+					$cats = $cats[0];
+				?>
+				<dd class="<?php echo $cats->category_nicename; ?>"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><?php the_excerpt(); ?></dd>
+				<?php endwhile; ?>
+			</dl>
+			<?php endif; ?>
+		</div>
+	</div>
 	<?php get_sidebar(); ?>
 </div>
 <?php get_footer(); ?>
