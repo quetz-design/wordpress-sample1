055d8e589881c5dd091c2875f9edf9ca887f22b8
Kouta Yoshimura <quetz.design@gmail.com>
2013-12-21 17:17:16 +0900

    chapter3 lesson01

--- wp-content/themes/company/header.php
+++ wp-content/themes/company/header.php
@@ -4,11 +4,28 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="Content-type" content="<?php bloginfo('html_type') ?>">
 	<title><?php bloginfo('name'); ?></title>
+	<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rss2_url'); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 <body>
 <!-- header -->
 <div id="header">
-	<h1><?php bloginfo('name') ?></h1>
-	<p><?php bloginfo('description'); ?></p>
+	<div id="header-inner">
+		<div id="header-top">
+			<div class="fLeft">
+				<h1><a href="<?php echo home_url('/'); ?>"><img src="<?php bloginfo('template_url'); ?>/images/logo.gif" alt="<?php bloginfo('name'); ?>"></a>
+				</h1>
+				<p><?php bloginfo('description'); ?></p>
+			</div>
+			<div class="fRight">
+				<ul>
+					<li><a href="<?php bloginfo('url'); ?>/sitemap/" class="sitemap">サイトマップ</a></li>
+					<li><a href="<?php bloginfo('rss2_url'); ?>" class="rss">RSS FEED</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="header-nav"><?php wp_nav_menu(array('theme_location' => 'nav')); ?></div>
+		<div id="header-gra"><img src="<?php bloginfo('template_url'); ?>/images/gra_main.jpg" alt=""></div>
+	</div>
 </div>
 <!-- /header -->
