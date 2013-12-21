af16dbbbf4a46e210f66f32b1856095a303f8f76
Kouta Yoshimura <quetz.design@gmail.com>
2013-12-21 15:45:52 +0900

    Lesson04 Step1

---
+++ wp-content/themes/company/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-type" content="<?php bloginfo('html_type'); ?>">
+	<title><?php bloginfo(); ?></title>
+</head>
+<body>
+	<h1><?php bloginfo('name'); ?></h1>
+	<p><?php bloginfo('description'); ?></p>
+	<ul>
+		<li><?php bloginfo('url'); ?></li>
+		<li><?php bloginfo('stylesheet_url'); ?></li>
+		<li><?php bloginfo('template_url'); ?></li>
+		<li><?php bloginfo('template_directory'); ?></li>
+		<li><?php bloginfo('charset'); ?></li>
+		<li><?php bloginfo('html_type'); ?></li>
+		<li><?php bloginfo('rss_url'); ?></li>
+		<li><?php bloginfo('rss2_url'); ?></li>
+		<li><?php bloginfo('rdf_url'); ?></li>
+		<li><?php bloginfo('atom_url'); ?></li>
+		<li><?php bloginfo('comments_rss2_url'); ?></li>
+		<li><?php bloginfo('pingback_url'); ?></li>
+		<li><?php bloginfo('wpurl'); ?></li>
+		<li><?php bloginfo('admin_email'); ?></li>
+		<li><?php bloginfo('version'); ?></li>
+	</ul>
+	<p>COPYRIGHT &copy; 2012 <?php bloginfo('name'); ?> ALLRIGHTS RESERVED</p>
+</body>
+</html>
