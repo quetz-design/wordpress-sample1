3423f1fc3c2a8b053b047a94deea08cfc25dc6d8
Kouta Yoshimura <quetz.design@gmail.com>
2013-12-21 16:22:03 +0900

    Lesson05 テンプレート分割

---
+++ wp-content/themes/company/footer.php
@@ -0,0 +1,7 @@
+<!-- footer -->
+<div id="footer">
+	<p>COPYRIGHT &copy; 2012 <?php bloginfo('name'); ?> ALLRIGHTS RESERVED</p>
+</div>
+<!-- /footer -->
+</body>
+</html>
---
+++ wp-content/themes/company/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-type" content="<?php bloginfo('html_type') ?>">
+	<title><?php bloginfo('name'); ?></title>
+</head>
+<body>
+<!-- header -->
+<div id="header">
+	<h1><?php bloginfo('name') ?></h1>
+	<p><?php bloginfo('description'); ?></p>
+</div>
+<!-- /header -->
