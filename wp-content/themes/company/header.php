e6f5b7e68d17e92b3a14ade7f8cb80709848ce35
Kouta Yoshimura <quetz.design@gmail.com>
2013-12-23 13:56:08 +0900

    chapter9 lesson2 ソーシャル連携追加

--- wp-content/themes/company/header.php
+++ wp-content/themes/company/header.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="Content-type" content="<?php bloginfo('html_type') ?>">
